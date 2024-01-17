300d0c51632352260df16825c3f64b14a85125dd
000xzhou <xzhou92@gmail.com>
2024-01-17 15:38:03 -0500

    added models

--- models.py
+++ models.py
@@ -1,7 +1,4 @@
 """Models for Blogly."""
-# - ***id***, an autoincrementing integer number that is the primary key
-# - ***first_name*** and ***last_name***    -require
-# - ***image_url*** for profile images      -defaults photo
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -9,15 +6,19 @@ db = SQLAlchemy()
 def connect_db(app):
     db.app = app
     db.init_app(app)
-class Blogly(db.Model):
-    __tablename__ = "Bloglys"
+
+class User(db.Model):
+    __tablename__ = "users"
+    
+    def __repr__(self):
+        s = self
+        return f"<pet id={s.id}, first name={s.first_name}, last name={s.last_name}>"
     
     id = db.Column(db.Integer,
                    primary_key=True,
                    autoincrement=True)
-    first_name = db.Colum(db.String(10),
+    first_name = db.Column(db.String(10),
+                           nullable=False)
+    last_name = db.Column(db.String(10),
                           nullable=False)
-    last_name = db.Colum(db.String(10),
-                          nullable=False)
-    # https://images.pexels.com/photos/69932/tabby-cat-close-up-portrait-69932.jpeg?cs=srgb&dl=pexels-pixabay-69932.jpg&fm=jpg
-    image_url = db.Colum(db.String(255), default='default.jpg')
+    image_url = db.Column(db.String(255), default='default.jpg')
