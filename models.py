339168f3d742ba40d172f9ecf931a9d7e90992bb
000xzhou <xzhou92@gmail.com>
2024-01-18 19:17:55 -0500

    added post to models

--- models.py
+++ models.py
@@ -26,3 +26,21 @@ class User(db.Model):
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+class Post(db.Model):
+    __tablename__ = "posts"
+    
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    title = db.Column(db.text, nullable=False)
+    content = db.Column(db.text, nullable=False)
+    # server_default=db.func.now() sets a default value of the current timestamp when a new post is created. 
+    created_at = db.Column(db.date, server_default=db.func.now())
+    user_id = db.ForeignKey(db.Integer, db.ForeignKey('users.id'))
+    
+    dept = db.relationship('User', backref='posts')
+    
+    def __repr__(self):
+        s = self
+        return f"<post id={s.id}, first name={s.first_name}, last name={s.last_name}, photo:{s.image_url}>"
+
