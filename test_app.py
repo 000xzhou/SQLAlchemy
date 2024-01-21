2779aadb032ddadc8307f8166d015b66625b42d5
000xzhou <xzhou92@gmail.com>
2024-01-21 15:07:20 -0500

    added post test

--- test_app.py
+++ test_app.py
@@ -13,7 +13,7 @@ class UserFlaskCase(TestCase):
         cls.app = app
         cls.app.config['TESTING'] = True
         cls.app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-        cls.app.config['SQLALCHEMY_ECHO'] = True
+        cls.app.config['SQLALCHEMY_ECHO'] = False
         cls.client = cls.app.test_client()
         with cls.app.app_context():
             db.drop_all()
@@ -37,16 +37,16 @@ class UserFlaskCase(TestCase):
     def tearDown(self):
         """Clean up instance-level resources."""
         with self.app.app_context():
+            User.query.delete()
             db.session.rollback()
-
-
-    def test_redirect(self):
+        
+    # check to seee if homepage loaded with no errors
+    def test_homepage(self):
         with self.app.test_client() as client:
             resp = client.get("/")
-            html = resp.get_data(as_text=True)
 
-            self.assertEqual(resp.status_code, 302)
-        
+            self.assertEqual(resp.status_code, 200)
+    
     # test to see if user is display
     def test_user_list(self):
         with self.app.test_client() as client:
@@ -95,8 +95,76 @@ class UserFlaskCase(TestCase):
             self.assertNotEqual("John Doe", html)
 class PostFlaskCase(TestCase):
     """Test for Post Cases"""
+    @classmethod
+    def setUpClass(cls):
+        """Set up Flask application context and configuration."""
+        cls.app = app
+        cls.app.config['TESTING'] = True
+        cls.app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+        cls.app.config['SQLALCHEMY_ECHO'] = True
+        cls.client = cls.app.test_client()
+        with cls.app.app_context():
+            db.drop_all()
+            db.create_all()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Clean up class-level resources."""
+        pass
+
+    def setUp(self):
+        """Add sample"""
+        with self.app.app_context():
+            Post.query.delete()
+            User.query.delete()
+            # Add a sample user
+            user = User(first_name="John", last_name="Doe", image_url="image_url.jpg")
+            db.session.add(user)
+            db.session.commit()
+            post = Post(title= "Lucky Star", content= "Little Star")
+            db.session.add(post)
+            db.session.commit()
+            self.user_id = user.id
+            self.post_id = post.id
+
+    def tearDown(self):
+        """Clean up instance-level resources."""
+        with self.app.app_context():
+            Post.query.delete()
+            User.query.delete()
+            db.session.rollback()
     
-    
+    # test new post form 
+    def test_post_form_submit(self):
+        with self.app.test_client() as client:
+            resp = client.post(f"/users/{self.user_id}/posts/new", data={"title": "Another", "content": "Star"}, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("Another", html)
+            
+    def test_post_detail_page(self):
+        with self.app.test_client() as client:
+            resp = client.get(f"/posts/{self.post_id}")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("Lucky Star", html) 
+            
+    def test_post_edit_page(self):
+        with self.app.test_client() as client:
+            resp = client.post(f"/users/{self.user_id}/posts/new", data={"title": "Another", "content": "Star"}, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("Another", html)
+            
+    def test_post_delete(self):
+        with self.app.test_client() as client:
+            resp = client.get(f"/posts/{self.post_id}/delete")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 302)
 class TagFlaskCase(TestCase):
     """Test for Tag Cases"""
     
